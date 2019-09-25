9406c23fd89ba90b7872dbf649f531b1550c0059
quentinverlhac <quentin.verlhac@gmail.com>
2019-09-25 16:07:40 +0200

    add grounded character check

---
+++ GravityJump/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private Transform groundedCheck;
+    [SerializeField] private LayerMask groundMask;
+
+    const float groundedRadius = 0.2f;
+
+    private bool isGrounded;
+
+    void FixedUpdate()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundedCheck.position, groundedRadius, groundMask);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                if (!wasGrounded)
+                    Debug.Log("Character has grounded");
+            }
+        }
+
+
+    }
+}
