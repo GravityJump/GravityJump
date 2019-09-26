e07017490918e32cd843ff17a5c6236e63bd4b11
Julien Doutre <jul.doutre@gmail.com>
2019-09-26 23:06:08 +0200

    Add a start screen

---
+++ GravityJump/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject TitleScreen;
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            this.TitleScreen.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ GravityJump/Assets/Scripts/UI/TItleScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TItleScreen : MonoBehaviour
+{
+    public Text press_start_text;
+    void Start()
+    {
+        StartCoroutine(this.ToggleVisibility());
+    }
+    private IEnumerator ToggleVisibility()
+    {
+        while (true)
+        {
+            this.press_start_text.enabled = false;
+            yield return new WaitForSeconds(0.7f);
+            this.press_start_text.enabled = true;
+            yield return new WaitForSeconds(0.7f);
+        }
+    }
+    void Update()
+    {
+
+    }
+}
