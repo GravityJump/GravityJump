3fedf7a4cda5acbba116bd091097723a567b5ac9
quentinverlhac <quentin.verlhac@gmail.com>
2019-09-26 20:18:19 +0200

    improve jump smoothness and flip character depending on direction

--- GravityJump/Assets/Scripts/CharacterController.cs
+++ GravityJump/Assets/Scripts/CharacterController.cs
@@ -9,24 +9,23 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private Collider2D closestPlanetoid;
     [SerializeField] private Collider2D characterCollider;
     [SerializeField] private float runSpeed = 7;
-    [SerializeField] private float jumpSpeed = 7;
 
     const float groundedRadius = 0.2f;
-    private float gravityStrengh = 50.0f;
-    private float jumpForce = 100f;
+    private float jumpForce = 10f;
+    private float gravityForce = 10f;
+    private float minGravitySpeedLimit = -10f;
 
     private Rigidbody2D rb2D;
+    private SpriteRenderer spriteRenderer;
     protected float horizontalSpeed;
+    private float verticalSpeed;
     protected bool jump;
     private bool isGrounded;
-    // For velocity smooth damp
-    private float smoothTime = .05f;
-    private Vector3 acceleration = Vector3.zero;
-
 
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     protected void FixedUpdate()
@@ -51,21 +50,26 @@ public class CharacterController : MonoBehaviour
             Debug.Log("Character has taken off");
         }
 
+        if (horizontalSpeed > 0.01f)
+            spriteRenderer.flipX = false;
+        else if (horizontalSpeed < -0.01f)
+            spriteRenderer.flipX = true;
+
         Move(horizontalSpeed, jump, Time.fixedDeltaTime);
     }
 
     protected void Move(float move, bool jump, float time)
     {
-        float verticalVelocity = -10f;
         ColliderDistance2D characterPlanetoidDistance = characterCollider.Distance(closestPlanetoid);
         Vector2 groundNormal = characterPlanetoidDistance.normal.normalized;
 
         if (jump)
         {
-            verticalVelocity = 0;
             if (isGrounded)
             {
-                rb2D.AddRelativeForce(new Vector2(0, 10), ForceMode2D.Impulse);
+                verticalSpeed = 0;
+                // Cancel gravity speed modifier and impulse force to jump
+                rb2D.AddRelativeForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
         else
@@ -73,12 +77,15 @@ public class CharacterController : MonoBehaviour
             // Reset velocity to avoid having remaining jump forces applied after jump has stopped
             rb2D.velocity = new Vector2();
         }
+        // Add gravity acceleration every time. Limit max speed to avoid extreme behaviors.
+        // We keep gravity acceleration after landed to stick the character to the ground.
+        verticalSpeed = Mathf.Max(verticalSpeed - rb2D.mass * gravityForce * time, minGravitySpeedLimit);
 
         transform.up = groundNormal;
 
         var moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
         Vector2 horizontalMove = move * runSpeed * moveAlongGround * time;
-        Vector2 verticalMove = verticalVelocity * groundNormal * time;
+        Vector2 verticalMove = verticalSpeed * groundNormal * time;
 
         rb2D.position = rb2D.position + horizontalMove + verticalMove;
     }
