fbbfd2f1df30a35d25f11a506395c982ed4d03d5
quentinverlhac <quentin.verlhac@gmail.com>
2019-10-03 14:42:26 +0200

    add second character representing enemy player

--- GravityJump/Assets/Scripts/GameObjects/Player.cs
+++ GravityJump/Assets/Scripts/GameObjects/Player.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 
 public abstract class Player : AttractableBody
 {
-    protected float runSpeed = 7;
-    protected bool isJumpEnabled = true;
-
     private string username;
 }
