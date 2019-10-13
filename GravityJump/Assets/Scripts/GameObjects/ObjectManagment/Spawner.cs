335e9a12a23dbc30d9f6e7f67d1c8cf91c7f8054
Hugo Saint-Vignes <hugo@Hugos-Air.mshome.net>
2019-10-13 21:39:35 +0200

    Added comments and fixed the spawner

--- GravityJump/Assets/Scripts/GameObjects/ObjectManagment/Spawner.cs
+++ GravityJump/Assets/Scripts/GameObjects/ObjectManagment/Spawner.cs
@@ -41,19 +41,23 @@ public class Spawner : MonoBehaviour
 
     void Start()
     {
+        // Defining spawner speed
         this.Speed = new Speed(2f);
-
+        // Computing the sum of available planets' frequency
         foreach (GameObject planet in this.AvailablePlanetsPrefabs)
         {
             total_frequency += planet.GetComponent<AttractiveBody>().frequency;
         }
-
+        // First positions x and y are already defined, preparing the next planet and it size
         ActivePlanet = GetRandomPlanet();
         r = ActivePlanet.GetComponent<AttractiveBody>().GetRandomSize();
     }
 
+    // Randomly select a Planet among the available ones.
     GameObject GetRandomPlanet()
     {
+        // We select a random number between 0 and the sum of available planets frequencies
+        // This number will define the selected planet
         float v = Random.value * total_frequency;
         float f;
         foreach (GameObject planet in this.AvailablePlanetsPrefabs)
@@ -68,43 +72,50 @@ public class Spawner : MonoBehaviour
                 v -= f;
             }
         }
-        // if nothing has been found
+        // In case nothing got selected 
+        // Should not happen as long as AvailablePlanetsPrefabs and their frequencies remain unchanged
         Debug.Log("No random planet could be selected");
         return this.AvailablePlanetsPrefabs[0];
     }
 
     void Update()
     {
+        // Spawner is moving
         transform.Translate(this.Speed.Value * Time.deltaTime, 0, 0);
-
+        // Checking if next planet position has been reached
         if (transform.position.x >= x)
         {
+            // Instanciate the Planet
             GeneratePlanet();
+            // Prepare next Planet
             PrepareNextSpawn();
         }
-
+        // Incrementing speed
         this.Speed.Increment(Time.deltaTime);
     }
 
+    // Instantiate the next planet with previously defined position and size
     void GeneratePlanet()
     {
-        // Rotation can be randomly decided on instantiation
+        // Rotation is randomly decided on instantiation
+        // x, y and r have been previously defined
         GameObject generatedObject = Instantiate(
             ActivePlanet,
             new Vector3(x, y, 0),
             Quaternion.Euler(0, 0, Random.value * 360));
         generatedObject.transform.localScale *= r;
         generatedObject.SetActive(true);
-
+        // @TODO : To comment
         if (!this.IsPlayerAlive)
         {
             this.PlayerSpawningPlanet = new SpawningPoint(generatedObject, x, y);
         }
     }
 
+    // Defined the next planet's position and size. Camera's y position is assumed to be 0.
     void PrepareNextSpawn()
     {
-        // Pulling previous usefull data on
+        // Pulling previous planet's data
         float r_last = r;
         float x_last = x;
         float y_last = y;
@@ -116,26 +127,44 @@ public class Spawner : MonoBehaviour
         float d_max = Mathf.Min(NextPlanet.GetComponent<AttractiveBody>().GetMaximalDistance(r), ActivePlanet.GetComponent<AttractiveBody>().GetMaximalDistance(r_last));
         // Deciding next planet distance to previous one
         float d = d_min + (d_max - d_min) * Random.value;
-        // Deducing minimal and maximal angle to the next planet (to avoid new planet out of limits)
-        float y_upper_limit = 5 + r / 2;
-        float y_lower_limit = -5 - r / 2;
-        float teta_upper_limit = 0;
-        float teta_lower_limit = Mathf.PI;
+        // Defining constraints for next planet's position
+        float y_limit = 5;
+        float angle_margin = Mathf.PI / 8;
+        float angle_min = Mathf.PI / 8;
+        float angle_max = 7 * Mathf.PI / 8;
+        // Planet can sometime appear slightly out of camera's range
+        if (Mathf.Abs(y_last) < 5)
+        {
+            y_limit += r / 3;
 
-        if (y_upper_limit < y_last + r_last + d + r)
-        {
-            teta_upper_limit += Mathf.Tan(r / (y_last + r_last + d + r - y_upper_limit));
         }
-        if (y_lower_limit > y_last - r_last - d - r)
+        // Finding an angle satisfying all constraints
+        float teta = angle_min;
+        int attempts = 0;
+        while (attempts < 20 && (teta <= angle_min || teta >= angle_max || Mathf.Abs(y) > y_limit || Mathf.Abs(teta - Mathf.PI / 2) < angle_margin))
         {
-            teta_lower_limit += Mathf.Tan(r / (y_last - r_last - d - r + y_lower_limit));
+            attempts += 1;
+            teta = angle_min + (angle_max - angle_min) * Random.value;
+            // Computing the y position of next planet
+            y = y_last + Mathf.Cos(teta) * (r_last + d + r);
         }
-        // Deciding next planet angle to previous one
-        float teta = teta_upper_limit + (teta_lower_limit - teta_upper_limit) * Random.value;
-        // Deducing the x and y position of next planet
+        // If no compromise could be made, bringing next planet closer to center of camera
+        if (attempts == 20)
+        {
+            Debug.Log("No random position could be selected");
+            if (y_last < 0)
+            {
+                teta = Mathf.PI / 4;
+            }
+            else
+            {
+                teta = 3 * Mathf.PI / 4;
+            }
+            // Computing the y position of next planet
+            y = y_last + Mathf.Cos(teta) * (r_last + d + r);
+        }
+        // Computing the x position of next planet
         x = x_last + Mathf.Sin(teta) * (r_last + d + r);
-        y = y_last + Mathf.Cos(teta) * (r_last + d + r);
-
         ActivePlanet = NextPlanet;
     }
 }
