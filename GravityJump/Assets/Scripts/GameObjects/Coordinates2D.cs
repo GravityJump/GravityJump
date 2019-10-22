eed8c2c959ea80085bcbdf781924c5e2a4d26a2c
quentinverlhac <quentin.verlhac@gmail.com>
2019-10-22 16:56:50 +0200

    add coordinates2D class

---
+++ GravityJump/Assets/Scripts/GameObjects/Coordinates2D.cs
@@ -0,0 +1,16 @@
+﻿namespace GameObjects
+{
+    public class Coordinates2D
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float ZAngle { get; set; }
+
+        public Coordinates2D(float x,float y, float zAngle)
+        {
+            this.X = x;
+            this.Y = y;
+            this.ZAngle = zAngle;
+        }
+    }
+}
