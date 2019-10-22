a0d19b83eec867d17f607782d97fcfb7543fa2ef
Julien Doutre <jul.doutre@gmail.com>
2019-10-22 14:01:15 +0200

    Fix time scale when pausing

--- GravityJump/Assets/Scripts/UI/PauseScreen.cs
+++ GravityJump/Assets/Scripts/UI/PauseScreen.cs
@@ -8,16 +8,19 @@ namespace UI
 
         public Button Back;
         public Button Resume;
+        float resumedTimeScale;
 
         public override void Awake()
         {
             this.Panel = GameObject.Find("GameController/HUD/PauseScreen/");
             this.Back = GameObject.Find("GameController/HUD/PauseScreen/BackButton").GetComponent<Button>();
             this.Resume = GameObject.Find("GameController/HUD/PauseScreen/ResumeButton").GetComponent<Button>();
+            this.resumedTimeScale = Time.timeScale;
         }
 
         public override void OnStart()
         {
+            this.resumedTimeScale = Time.timeScale;
             Time.timeScale = 0f;
             this.Panel.SetActive(true);
         }
@@ -25,7 +28,7 @@ namespace UI
         public override void OnStop()
         {
             this.Panel.SetActive(false);
-            Time.timeScale = 1f;
+            Time.timeScale = this.resumedTimeScale;
         }
     }
 }
