ba6df35093e6faa6f39ba110b4012629ddd93f06
quentinverlhac <quentin.verlhac@gmail.com>
2019-10-22 17:36:37 +0200

    [temp commit] move opponent smoothly based on last position received

--- GravityJump/Assets/Scripts/GameObjects/OpponentPlayer.cs
+++ GravityJump/Assets/Scripts/GameObjects/OpponentPlayer.cs
@@ -2,7 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClientPlayer : Player
+public class OpponentPlayer : Body
 {
-    // Mapping UDP reception to MoveUp, ... methods
+    public GameObjects.Coordinates2D coordinates2D;
+
+    private void Awake()
+    {
+        this.coordinates2D = new GameObjects.Coordinates2D(0, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = Vector2.Lerp(this.transform.position, this.coordinates2D.getVector2(), 0.1f);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.AngleAxis(this.coordinates2D.ZAngle, Vector3.forward), 0.01f);
+        coordinates2D.X += 0.02f;
+        coordinates2D.Y += 0.002f;
+        coordinates2D.ZAngle += 2f;
+    }
 }
