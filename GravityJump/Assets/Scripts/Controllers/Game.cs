872ceeaf46f9e31a50806ffb0aea378b18692a6b
Quentin VERLHAC <quentin@mbp-quentin.home>
2019-11-02 15:06:49 +0100

    regularly send local player positions to other player

--- GravityJump/Assets/Scripts/Controllers/Game.cs
+++ GravityJump/Assets/Scripts/Controllers/Game.cs
@@ -17,6 +17,8 @@ namespace Controllers
         private List<ObjectManagement.Spawner> Spawners { get; set; }
         private Physic.Speed Speed { get; set; }
         private bool IsHost { get; set; }
+        private float PositionSendingFrequency { get; set; } // The number of position message sent per second
+        private float TimeSinceLastPositionSending { get; set; } 
 
         private void Awake()
         {
@@ -35,12 +37,16 @@ namespace Controllers
 
             this.Connection = Data.Storage.Connection; // Will be null in a solo game.
             this.IsHost = Data.Storage.IsHost;
+            this.PositionSendingFrequency = 10f;
+            this.TimeSinceLastPositionSending = 0f;
 
             this.Speed = new Physic.Speed(1f);
         }
 
         private void Update()
         {
+            this.TimeSinceLastPositionSending += Time.deltaTime;
+
             // Instantiate the local player if not already done.
             if (this.LocalPlayerSpawner.PlayerObject == null && this.PlanetSpawner.PlayerSpawningPlanet != null)
             {
@@ -49,15 +55,7 @@ namespace Controllers
                 // Send the player position to the client if in a multiplayer game.
                 if (this.Connection != null)
                 {
-                    this.Connection.Write(
-                        new Network.PlayerCoordinates(
-                            new Physic.Coordinates2D(
-                                this.LocalPlayerSpawner.PlayerObject.transform.position.x,
-                                this.LocalPlayerSpawner.PlayerObject.transform.position.y,
-                                this.LocalPlayerSpawner.PlayerObject.transform.rotation.eulerAngles.z
-                            )
-                        )
-                    );
+                    this.SendLocalPlayerPosition();
                 }
             }
 
@@ -121,6 +119,13 @@ namespace Controllers
                             break;
                     }
                 }
+
+                // ...and send local player position to the other player
+                if (this.TimeSinceLastPositionSending >= 1 / this.PositionSendingFrequency)
+                {
+                    this.SendLocalPlayerPosition();
+                }
+
             }
 
             this.transform.Translate(this.Speed.Value * Time.deltaTime, 0, 0); // `transform` is a field of `MonoBehaviour`.
@@ -139,6 +144,23 @@ namespace Controllers
                 // @TODO: Game Over animation
                 Data.Storage.LocalScore = this.HUD.Distance;
                 SceneManager.LoadScene("Menu");
+            }
+        }
+
+        private void SendLocalPlayerPosition()
+        {
+            if (this.Connection != null)
+            {
+                this.Connection.Write(
+                    new Network.PlayerCoordinates(
+                        new Physic.Coordinates2D(
+                            this.LocalPlayerSpawner.PlayerObject.transform.position.x,
+                            this.LocalPlayerSpawner.PlayerObject.transform.position.y,
+                            this.LocalPlayerSpawner.PlayerObject.transform.rotation.eulerAngles.z
+                        )
+                    )
+                );
+                this.TimeSinceLastPositionSending = 0f;
             }
         }
 
