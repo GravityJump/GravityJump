8b6d6d1844975a49ac1049de3e1a24ea04bdef15
quentinverlhac <quentin.verlhac@gmail.com>
2019-11-07 17:32:55 +0100

    implement PlayAnimation function that manage animations type and framerate

--- GravityJump/Assets/Scripts/Animation/PlayerAnimator.cs
+++ GravityJump/Assets/Scripts/Animation/PlayerAnimator.cs
@@ -8,12 +8,19 @@ namespace Animation
     {
         private Physic.AttractableBody AttractableBody;
         // This enum represent animation types. They must be named after the animation name in the dictionary (and the folder name in file system)
-        private enum AnimationTypes
+        private enum AnimationType
         {
             Idle,
-            Walk,
-            Jump,
+            Walking,
+            Jumping,
+            InFlight,
+            Falling,
+            Landing,
         }
+        private AnimationType currentAnimationPlayed;
+        private const float SecondPerImage = 1/12;
+        private float TimeSinceLastImage;
+        private int currentFrameIndex;
 
         protected override string GameObjectAnimationsDirectoryName => "Player";
 
@@ -25,23 +32,48 @@ namespace Animation
 
         private void Update()
         {
+            TimeSinceLastImage += Time.deltaTime;
             switch (AttractableBody.playerMovingState.movingState)
             {
                 case Physic.PlayerMovingState.MovingState.Grounded:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Idle.ToString("g")][0];
+                    this.PlayAnimation(AnimationType.Idle);
+                    break;
+                case Physic.PlayerMovingState.MovingState.Walking:
+                    this.PlayAnimation(AnimationType.Walking);
                     break;
                 case Physic.PlayerMovingState.MovingState.Jumping:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump.ToString("g")][1];
+                    this.PlayAnimation(AnimationType.Jumping);
                     break;
                 case Physic.PlayerMovingState.MovingState.InFlight:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump.ToString("g")][2];
+                    this.PlayAnimation(AnimationType.InFlight);
                     break;
                 case Physic.PlayerMovingState.MovingState.Falling:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump.ToString("g")][3];
+                    this.PlayAnimation(AnimationType.Falling);
                     break;
                 case Physic.PlayerMovingState.MovingState.Landing:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump.ToString("g")][1];
+                    this.PlayAnimation(AnimationType.Landing);
                     break;
+            }
+        }
+
+        private void PlayAnimation(AnimationType type)
+        {
+            Sprite[] animationSprites = Animations[type.ToString("g")];
+            if (currentAnimationPlayed == type)
+            {
+                if (TimeSinceLastImage >= SecondPerImage)
+                {
+                    currentFrameIndex = (currentFrameIndex + 1) % animationSprites.Length;
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = animationSprites[currentFrameIndex];
+                    TimeSinceLastImage = 0;
+                }
+
+            } else
+            {
+                currentAnimationPlayed = type;
+                currentFrameIndex = 0;
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = animationSprites[currentFrameIndex];
+                TimeSinceLastImage = 0;
             }
         }
     }
