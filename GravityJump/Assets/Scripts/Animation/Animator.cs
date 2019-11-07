cfd163e99f4046252d6d95fe39c5dd9ed8922029
quentinverlhac <quentin.verlhac@gmail.com>
2019-11-07 15:42:18 +0100

    add animator abstract class

---
+++ GravityJump/Assets/Scripts/Animation/Animator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Animation
+{
+    public abstract class Animator : MonoBehaviour
+    {
+        protected abstract string AnimationDirectoryPath { get; }
+        // Store the different animation types and their sprites in a key value pair data structure
+        protected Dictionary<string, Sprite[]> Animations;
+
+        protected void Awake()
+        {
+            this.Animations = new Dictionary<string, Sprite[]>();
+
+            // Load sprites
+            foreach (string animationDirectory in Directory.GetDirectories($"Assets/Resources/{AnimationDirectoryPath}"))
+            {
+                // Get the name of the directory, without the path
+                string animationType = new DirectoryInfo(animationDirectory).Name;
+                string path = $"{AnimationDirectoryPath}{animationType}";
+                this.Animations.Add(animationType, Resources.LoadAll<Sprite>(path));
+            }
+        }
+    }
+}
--- GravityJump/Assets/Scripts/Animation/PlayerAnimator.cs
+++ GravityJump/Assets/Scripts/Animation/PlayerAnimator.cs
@@ -4,29 +4,16 @@ using UnityEngine;
 
 namespace Animation
 {
-    public class PlayerAnimator : MonoBehaviour
+    public class PlayerAnimator : Animator
     {
         private Physic.AttractableBody AttractableBody;
-        // Store the different animation types and their sprites in a key value pair data structure
-        private Dictionary<AnimationTypes, Sprite[]> Animations;
 
-        private enum AnimationTypes
+        protected override string AnimationDirectoryPath => "Animation/Player/";
+
+        private new void Awake()
         {
-            Jump,
-        }
-
-        private void Awake()
-        {
-            this.Animations = new Dictionary<AnimationTypes, Sprite[]>();
-
-            // Load sprites
-            foreach (AnimationTypes animationType in Enum.GetValues(typeof(AnimationTypes)))
-            {
-                string path = $"Animation/Player/{animationType.ToString("g")}";
-                this.Animations.Add(animationType, Resources.LoadAll<Sprite>(path));
-            }
-
             AttractableBody = this.gameObject.GetComponent<Physic.AttractableBody>();
+            base.Awake();
         }
 
         private void Update()
@@ -34,19 +21,19 @@ namespace Animation
             switch (AttractableBody.playerMovingState.movingState)
             {
                 case Physic.PlayerMovingState.MovingState.Grounded:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump][0];
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations["Jump"][0];
                     break;
                 case Physic.PlayerMovingState.MovingState.Jumping:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump][1];
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations["Jump"][1];
                     break;
                 case Physic.PlayerMovingState.MovingState.InFlight:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump][2];
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations["Jump"][2];
                     break;
                 case Physic.PlayerMovingState.MovingState.Falling:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump][3];
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations["Jump"][3];
                     break;
                 case Physic.PlayerMovingState.MovingState.Landing:
-                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations[AnimationTypes.Jump][1];
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = Animations["Jump"][1];
                     break;
             }
         }
