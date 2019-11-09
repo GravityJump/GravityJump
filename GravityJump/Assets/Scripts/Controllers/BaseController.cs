cbb177dd70038dc3f5ee7891bcfb8eaea1d6acaf
quentinverlhac <quentin.verlhac@gmail.com>
2019-11-09 13:12:33 +0100

    add base controller abstract class

---
+++ GravityJump/Assets/Scripts/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Controllers
+{
+    // This abstract class implements the commun logic of all controllers
+    public abstract class BaseController : MonoBehaviour
+    {
+        protected Audio.MusicPlayer MusicPlayer { get; set; }
+        protected Network.Connection Connection { get; set; }
+
+        protected void Awake()
+        {
+            this.MusicPlayer = this.gameObject.AddComponent<Audio.MusicPlayer>();
+        }
+    }
+}
