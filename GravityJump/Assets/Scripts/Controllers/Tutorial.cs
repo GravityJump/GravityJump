dc49f33275edf83f67bc43cfdc87d019a3cd9558
Julien Doutre <jul.doutre@gmail.com>
2019-11-21 15:01:33 +0100

    Add empty tutorial scene

---
+++ GravityJump/Assets/Scripts/Controllers/Tutorial.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Text;
+
+namespace Controllers
+{
+    public class Tutorial : BaseController
+    {
+        private new void Awake()
+        {
+
+        }
+
+        private void Start()
+        {
+
+        }
+    }
+}
