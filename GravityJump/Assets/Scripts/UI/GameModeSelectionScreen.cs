1a0094c12f57b872234a2bfe3b41adc1fde2a2e6
Julien Doutre <jul.doutre@gmail.com>
2019-11-21 14:51:57 +0100

    Add logic to switch to tuto scene

--- GravityJump/Assets/Scripts/UI/GameModeSelectionScreen.cs
+++ GravityJump/Assets/Scripts/UI/GameModeSelectionScreen.cs
@@ -6,24 +6,25 @@ namespace UI
 {
     public class GameModeSelectionScreen : BasicScreen
     {
-        public Button SoloButton { get; set; }
+        private Button TutoButton { get; set; }
+        private Button SoloButton { get; set; }
+        private Button ExitButton { get; set; }
         public Button HostButton { get; set; }
         public Button JoinButton { get; set; }
         public Button CreditsButton { get; set; }
-        private Button ExitButton { get; set; }
-        private Text VersionText { get; set; }
         private Text Ip { get; set; }
 
         public override void Awake()
         {
+            this.Name = Names.Menu.GameModeSelection;
             this.Panel = GameObject.Find("Canvas/GameModeSelectionScreen");
             this.Ip = GameObject.Find("Canvas/GameModeSelectionScreen/Ip").GetComponent<Text>();
+            this.TutoButton = GameObject.Find("Canvas/GameModeSelectionScreen/TutoButton").GetComponent<Button>();
             this.SoloButton = GameObject.Find("Canvas/GameModeSelectionScreen/SoloButton").GetComponent<Button>();
             this.HostButton = GameObject.Find("Canvas/GameModeSelectionScreen/HostButton").GetComponent<Button>();
             this.JoinButton = GameObject.Find("Canvas/GameModeSelectionScreen/JoinButton").GetComponent<Button>();
-            this.ExitButton = GameObject.Find("Canvas/GameModeSelectionScreen/ExitButton").GetComponent<Button>();
             this.CreditsButton = GameObject.Find("Canvas/GameModeSelectionScreen/CreditsButton").GetComponent<Button>();
-            this.Name = Names.Menu.GameModeSelection;
+            this.ExitButton = GameObject.Find("Canvas/GameModeSelectionScreen/ExitButton").GetComponent<Button>();
         }
 
         public void Start()
@@ -36,6 +37,10 @@ namespace UI
             this.ExitButton.onClick.AddListener(() =>
             {
                 Application.Quit();
+            });
+            this.TutoButton.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("Tutorial");
             });
         }
 
