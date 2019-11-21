7af9f52fa5aa062d27a9226f49b4966e1a92ec5c
Hugo SV <hug.stv@gmail.com>
2019-11-21 20:36:42 +0000

    Add different logic if MAC

--- GravityJump/Assets/Scripts/Network/Utils.cs
+++ GravityJump/Assets/Scripts/Network/Utils.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
+using UnityEngine;
 
 namespace Network
 {
@@ -9,6 +10,23 @@ namespace Network
     {
         public static string GetHostIpAddress()
         {
+            if (SystemInfo.operatingSystem.Contains("Mac"))
+            {
+                foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (ni.Name == "en0")
+                    {
+                        foreach (System.Net.NetworkInformation.UnicastIPAddressInformation item in ni.GetIPProperties().UnicastAddresses)
+                        {
+                            if (item.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                return item.Address.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
             System.Net.IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (IPAddress ip in host.AddressList)
             {
@@ -23,7 +41,7 @@ namespace Network
 
         public static bool IsInternetAvailable()
         {
-            Ping pinger = new Ping();
+            System.Net.NetworkInformation.Ping pinger = new System.Net.NetworkInformation.Ping();
             try
             {
                 return pinger.Send("1.1.1.1").Status == IPStatus.Success;
