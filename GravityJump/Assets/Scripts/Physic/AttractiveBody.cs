1869a16b278a935d4a4c660d350367ee5be0b185
quentinverlhac <quentin.verlhac@gmail.com>
2019-11-26 16:45:00 +0100

    clean attractive body

--- GravityJump/Assets/Scripts/Physic/AttractiveBody.cs
+++ GravityJump/Assets/Scripts/Physic/AttractiveBody.cs
@@ -3,43 +3,19 @@ using UnityEngine;
 
 namespace Physic
 {
+    /**
+     * AttractiveBody provides getters for planet physics.
+    **/
     public class AttractiveBody : PhysicBody
     {
-        public static List<AttractiveBody> ActiveAttractiveBodies;
+        public float default_size;
         public float Frequency;
+
         // All following colliders are attached to children of the current gameObject.
         // A rigidbody collider that represent the ground on which the player will walk.
         public Collider2D ground;
         // A trigger collider, slightly below the ground collider. It is used to compute the normal to the ground.
         public Collider2D normalShape;
-        public int id { get; private set; }
-        private static int idGenerator;
-
-        public float default_size;
-
-        AttractiveBody()
-        {
-            id = idGenerator;
-            idGenerator++;
-        }
-
-        static AttractiveBody()
-        {
-            ActiveAttractiveBodies = new List<AttractiveBody>();
-        }
-
-        public AttractiveBody(bool isActive)
-        {
-            if (isActive)
-            {
-                AttractiveBody.ActiveAttractiveBodies.Add(this);
-            }
-        }
-
-        public Vector2 getAttractiveForce(AttractableBody ab)
-        {
-            return new Vector2(0, 0);
-        }
 
         // Give a random size for the spawner
         public float GetRandomSize()
@@ -47,17 +23,18 @@ namespace Physic
             return default_size * (Random.value * 6f + 1.2f);
         }
         // For a given size, return Minimal distance an adjacent planet can be
-        public float GetMinimalDistance(float size)
+        public float GetMinimalDistance()
         {
-            return 2; //size;
+            return 2;
         }
 
         // For a given size, Return Maximal distance an adjacent planet can be
-        public float GetMaximalDistance(float size)
+        public float GetMaximalDistance()
         {
-            return 4; //2 * size;
+            return 4;
         }
 
+        // getDistanceBetweenNormalAndGround returns a ColliderDistance2D representing the distance between the normal collider and the ground collider
         public ColliderDistance2D getDistanceBetweenNormalAndGround()
         {
             return normalShape.Distance(ground);
