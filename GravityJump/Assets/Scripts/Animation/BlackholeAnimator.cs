08005958fe8bf3bc4475dc9d3d314cde95085c38
quentinverlhac <quentin.verlhac@gmail.com>
2019-11-26 19:01:39 +0100

    fix blackhole rotation

--- GravityJump/Assets/Scripts/Animation/BlackholeAnimator.cs
+++ GravityJump/Assets/Scripts/Animation/BlackholeAnimator.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Animation
 {
     public class BlackholeAnimator : Animator
     {
+        // Sprites for each animation
+        public Sprite[] Rotation;
+
+        private new void Awake()
+        {
+            // Load Sprites
+            this.Animations = new Dictionary<string, Sprite[]>
+            {
+                { "Rotation", Rotation },
+            };
+        }
+
         private void Update()
         {
             TimeSinceLastImage += Time.deltaTime;
